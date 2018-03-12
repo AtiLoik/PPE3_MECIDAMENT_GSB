a013cd236cd373bcc0b2e471c7b6b80c19dd8b1b
Xissamine <32639781+Xissamine@users.noreply.github.com>
2018-03-12 16:19:57 +0100

    fix ajout médoc

--- controleurs/c_Medicament.php
+++ controleurs/c_Medicament.php
@@ -17,6 +17,11 @@ switch($action) //suivant le contrôleur dans uc
 	case 'Afficher' :
 		include("vues/Liste_Med.php");
 		break;
+
+	case 'VerifForm' :
+					Medicaments::ajouter($_POST['MED_DEPOTLEGAL'],$_POST['FAM_CODE'],$_POST['MED_NOMCOMMERCIAL'],$_POST['MED_COMPOSITION'],$_POST['MED_EFFETS'],$_POST['MED_CONTREINDIC'],$_POST['MED_PRIXECHANTILLON']);
+					header("refresh: 0;url=index.php?uc=Medicament&action=Afficher ");
+		break;
 }
 include("vues/pied.php") ;// pied de site
 /*include ("vues/entete.php");
